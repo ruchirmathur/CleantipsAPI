f400f5e082f3af32f736cde57558b1490d7d69da
Ruchir <ruchirmat@gmail.com>
2019-08-17 08:54:25 -0700

    Added health check for the service

---
+++ src/main/java/com/cleantips/api/HealthIndicators.java
@@ -0,0 +1,14 @@
+package com.cleantips.api;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HealthIndicators implements HealthIndicator {
+
+	@Override
+	public Health health() {
+		return Health.up().build();
+	}
+
+}
